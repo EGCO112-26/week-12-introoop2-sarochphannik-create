7b5ed19284c0a16d09d4f1740ef511fe7336a6fe
Sin(guy)/Cos(guy) <sarochphan.nik@student.mahidol.edu>
2026-03-23 09:31:01 +0000

    files added

--- main.cpp
+++ main.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 #include "student.h"
-#define N 3
 int main(int argc, char* argv[]) {
-  student s1,s2;
-
- s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
-
-
-
-  student a[N];
-  // Set age and set name for the first two ppl
-  int i,age;
-  string n;
-   // 1. Ask for N names and N age using cin
- 
-  
- 
-  //2. Print name and age of all N ppl
-  
-  
-  
-  //3. Print name and age of 
-  //the youngest person
-  //Find what the age of the youngest person
- 
-  
-  // Print all info for the yougest person
- 
-  
-  
-  //4. Change input from cin to argv
-
-  
- return 0;
-
-    
+  if (argc < 3 || (argc - 1) % 2 != 0) {
+    cout << "Usage: ./student <name1> <age1> [<name2> <age2> ...]" << endl;
+    return 1;
   }
+
+  int youngest_age = 0;
+  bool first_person = true;
+
+  for (int i = 1; i < argc; i += 2) {
+    int age = stoi(argv[i + 1]);
+
+    if (first_person || age < youngest_age) {
+      youngest_age = age;
+      first_person = false;
+    }
+  }
+
+  cout << "All persons:" << endl;
+  for (int i = 1; i < argc; i += 2) {
+    student s;
+    s.set_name(argv[i]);
+    s.set_age(stoi(argv[i + 1]));
+    s.display();
+  }
+
+  cout << "Youngest person(s):" << endl;
+  for (int i = 1; i < argc; i += 2) {
+    int age = stoi(argv[i + 1]);
+    if (age == youngest_age) {
+      student s;
+      s.set_name(argv[i]);
+      s.set_age(age);
+      s.display();
+    }
+  }
+
+  return 0;
+}
   
   
 
--- student.h
+++ student.h
@@ -5,10 +5,27 @@ class student{
 private:
 
   string name;
+  int age;
 
 public:
   
-   void set_name(string="Yme");
-    void print_name();
+  void set_name(string n="Yme") {
+   name = n;
+  }
 
+  void set_age(int a) {
+   age = a;
+  }
+
+  int get_age() {
+   return age;
+  }
+
+  void print_name() {
+   cout << name;
+  }
+
+  void display() {
+   cout << name << " " << age << endl;
+  }
 };
